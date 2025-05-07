815e85e04a670ff733bf54b12ea264d04f1494b5
Manuel Schneider <manuelschneid3r@gmail.com>
2025-05-07 14:25:08 +0200

    Adopt API changes

--- src/plugin.mm
+++ src/plugin.mm
@@ -15,6 +15,7 @@
 ALBERT_LOGGING_CATEGORY("dictionary")
 using namespace albert;
 using namespace std;
+using namespace util;
 #if  ! __has_feature(objc_arc)
 #error This file must be compiled with ARC.
 #endif
